760c5c0c96a3d75bc81a3528e3f52bdb4c268dce
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-07 19:19:18 +0000

    Change: setup of app views.py, and project urls/settings.py to ready first deployment

--- jhoanCodes/settings.py
+++ jhoanCodes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eyqh++5x3bop_k561geaep$zdflix#f7d0ktjcpkbhfnl(msp-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'blog',
 ]
 
 MIDDLEWARE = [
