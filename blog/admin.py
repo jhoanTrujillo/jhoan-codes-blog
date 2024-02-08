286b0f58238b078a710186ed2bcc91a524da47a5
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-08 22:15:14 +0000

    Update(blog/admin.py): register comment model into admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Registering the model into the admin
 # this give us CRUD capabilities with the post
 admin.site.register(Post)
+admin.site.register(Comment)
