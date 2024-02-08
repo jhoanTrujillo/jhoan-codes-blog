0e74a471e7fcdf5de545a92af9bd96fb4d94479b
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-08 19:32:07 +0000

    Change: post model to include an excerpt

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Options to show text draft or published
+# in the blog creation area of the admin
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
@@ -11,6 +13,11 @@ class Post(models.Model):
 	author = models.ForeignKey(
 		User, on_delete=models.CASCADE, related_name="blog_posts"
 	)
+	# max_length set to 320 to limit the number of lines
+	# that can be display, allowing for 80 chars per line
+	# blank allows for empty value submittion
+	excerpt = models.TextField(max_length=320, blank=True)
 	content = models.TextField()
+	# auto_now_add will add the creation date of the post to the database
 	created_on = models.DateTimeField(auto_now_add=True)
 	status = models.IntegerField(choices=STATUS, default=0)
