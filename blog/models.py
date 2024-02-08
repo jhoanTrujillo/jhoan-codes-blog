240ad1cc79f46a76c6b3f814380e5b179c7263db
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-08 22:10:51 +0000

    Update(blog/models.py): create comment model for blog post

--- blog/models.py
+++ blog/models.py
@@ -22,3 +22,14 @@ class Post(models.Model):
 	created_on = models.DateTimeField(auto_now_add=True)
 	updated_on = models.DateTimeField(auto_now=True)
 	status = models.IntegerField(choices=STATUS, default=0)
+
+class Comment(models.Model):
+	post = models.ForeignKey(
+		Post, on_delete=models.CASCADE, related_name="comments"
+	)
+	author = models.ForeignKey(
+		User, on_delete=models.CASCADE, related_name="commenter"
+	)
+	body = models.TextField()
+	approval = models.BooleanField(default=False)
+	created_on = models.DateTimeField(auto_now_add=True)
