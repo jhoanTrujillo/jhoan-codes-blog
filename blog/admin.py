e87f9a072dcf823c2e85743c7199ff98929cb61d
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-10 10:11:17 +0000

    Change: replace post model registration with post class from summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Registering the model into the admin
-# this give us CRUD capabilities with the post
-admin.site.register(Post)
+# Decorator needed to register a class instead of a plain model
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+	list_display = ('title', 'slug', 'status')
+	search_fields = ['title']
+	list_filter = ('status',)
+	prepopulated_fields = {'slug': ('title',)}
+	summernote_fields = ('content',)
+
+# Models should be register below
 admin.site.register(Comment)
