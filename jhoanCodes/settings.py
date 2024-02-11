9ff003c8ffada6a9bce18e089b93d723c25d6094
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-11 18:52:17 +0000

    Add: allauth variables, middleware, and apps to settings

--- jhoanCodes/settings.py
+++ jhoanCodes/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com',]
 
@@ -40,10 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	# Added after creation
-	'blog',
+	# Django allauth apps
+	'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+	# Summernote app
 	'django_summernote',
+	'blog',
 ]
+
+# 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	# Allauth django app middleware
+	'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'jhoanCodes.urls'
@@ -110,6 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
