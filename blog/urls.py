853a63f6521531afdfe7610749426e7e15f534c7
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-11 18:01:14 +0000

    Change: urlpatterns for app to better reflect navigation

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 # List of urls that will be used by the app
 # this list is taken by the project level urls.py file
 urlpatterns = [
-	path('', views.PostList.as_view(), name='post_list'),
-	path('<slug:slug>/', views.post_view, name='post_view'),
+	path('', views.IndexPage.as_view(), name='home'),
+	path('blog/post_list', views.PostList.as_view(), name='blog_list'),
+	path('blog/post_list/<slug:slug>/', views.post_view, name='post_view'),
 ]
