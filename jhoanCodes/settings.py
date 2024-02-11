c3b3a09a397e82ee29c3e7ba1c4ef7b4ce6a205b
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-11 08:46:06 +0000

    Add: files needed to deploy staticfiles to heroku

--- jhoanCodes/settings.py
+++ jhoanCodes/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	# Add white noise middleware to app for static file deployment
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
