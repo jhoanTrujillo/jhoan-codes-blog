8a6db51bac96e1445b4304d3a44230bf99d9f008
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-11 10:09:45 +0000

    Add: post_view template and setup url routes for single post page

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 # List of urls that will be used by the app
 # this list is taken by the project level urls.py file
 urlpatterns = [
-	path('posts/', views.PostList.as_view(),
-	name='post-urls'),
+	path('', views.PostList.as_view(), name='post_list'),
+	path('<slug:slug>/', views.post_view, name='post_view'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,24 @@ from django.views import generic
 class PostList(generic.ListView):
 	# Import model to access data via generic view
 	# Filtering results by published post only
-	queryset = Post.objects.filter(status=0).order_by("created_on")
+	queryset = Post.objects.filter(status=1).order_by("created_on")
 	template_name = 'blog/index.html'
 	paginate_by = 6
+
+def post_view(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+    **Context**
+    ``post``
+        An instance of :model:`blog.Post`.
+    **Template:**
+    :template:`blog/post_detail.html`
+    """
+    # Should change before production as status 0 reference draft post
+    queryset = Post.objects.filter(status=0)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(
+        request,
+        "blog/post_view.html",
+        {"post": post},
+    )
