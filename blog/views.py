0fa8cfa2c52e7d5fb27e86966727f82314f8810e
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-02-11 18:00:11 +0000

    Change: html use by PostList class and create IndexPage class for views

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Imports generic class from django
 from django.views import generic
 
 
 # Class based view 
+class IndexPage(generic.ListView):
+	# Import model to access data via generic view
+	# Filtering results by published post only
+	queryset = Post.objects.filter(status=1).order_by("-created_on")
+	template_name = 'blog/index.html'
+	paginate_by = 6
+      
 class PostList(generic.ListView):
 	# Import model to access data via generic view
 	# Filtering results by published post only
-	queryset = Post.objects.filter(status=1).order_by("created_on")
-	template_name = 'blog/index.html'
-	paginate_by = 6
+	queryset = Post.objects.filter(status=1).order_by("-created_on")
+	template_name = 'blog/post_list.html'
+	paginate_by = 9
 
 def post_view(request, slug):
     """
@@ -22,7 +29,7 @@ def post_view(request, slug):
     :template:`blog/post_detail.html`
     """
     # Should change before production as status 0 reference draft post
-    queryset = Post.objects.filter(status=0)
+    queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     return render(
         request,
